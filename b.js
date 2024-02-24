815aefe9087b48d765139ee476a9b6e642ee6a8e
Bing Shulin <shulin@skiff.com>
2024-02-24 15:34:31 +0100

    Added CNAME.txt

--- b.js
+++ b.js
@@ -5,24 +5,29 @@ function updateContent(language) {
 	switch(language) {
 	    case "en":
 		document.getElementById("navbarlink1text").textContent = "About us";
+		document.getElementById("navbarlink2text").textContent = "Services";
+		document.getElementById("navbarlink3text").textContent = "Pricing";
+		document.getElementById("navbarlink4text").textContent = "Gei sex gallery";
+		document.getElementById("navbarlink5text").textContent = "Free V-Bucks";
+
 		document.getElementById("navbarbuttontext").textContent = "Contact Us";
+
 		document.getElementById("footerTOStext").textContent = "Terms of Service";
 		document.getElementById("footerPPtext").textContent = "Privacy Policy";
 		document.getElementById("footerrightstext").textContent = "All rights reserved";
 		break;
 	    case "cs":
 		document.getElementById("navbarlink1text").textContent = "O nás";
-		document.getElementById("navbarbuttontext").textContent = "Kontaktuje nás";
+		document.getElementById("navbarlink2text").textContent = "Služby";
+		document.getElementById("navbarlink3text").textContent = "Ceník";
+		document.getElementById("navbarlink4text").textContent = "Gei sex galerie";
+		document.getElementById("navbarlink5text").textContent = "V-Bucks zadarmo";
+
+		document.getElementById("navbarbuttontext").textContent = "Kontaktujte nás";
+
 		document.getElementById("footerTOStext").textContent = "Smluvní podmínky";
 		document.getElementById("footerPPtext").textContent = "Ochrana osobních údajů";
 		document.getElementById("footerrightstext").textContent = "Všechna práva vyhrazena";
-		break;
-	    default:
-		// Set default language (English)
-		document.getElementById("paragraph1").textContent = "English content";
-		document.getElementById("paragraph2").textContent = "Another English content";
-		document.getElementById("link1").textContent = "English link";
-		document.getElementById("link2").textContent = "Another English link";
 		break;
 	}
 }
