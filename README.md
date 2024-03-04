b1ad667e16f66fd776cb7649bb884da8021d7300
Bing Shulin <shulin@skiff.com>
2024-03-04 17:25:23 +0100

    Created placeholder

--- README.md
+++
@@ -1,1 +0,0 @@
-A uni course project sample website to flex on non-tech classmates. (I am one of them :mike-wazowski-sulley-face-swap:)
--- b.js
+++ b.js
@@ -4,25 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
 function updateContent(language) {
 	switch(language) {
 	    case "en":
-		document.getElementById("navbarlink1text").textContent = "About us";
-		document.getElementById("navbarlink2text").textContent = "Services";
-		document.getElementById("navbarlink3text").textContent = "Pricing";
-		document.getElementById("navbarlink4text").textContent = "Testimonials";
-
-		document.getElementById("navbarbuttontext").textContent = "Contact Us";
-
 		document.getElementById("footerTOStext").textContent = "Terms of Service";
 		document.getElementById("footerPPtext").textContent = "Privacy Policy";
 		document.getElementById("footerrightstext").textContent = "All rights reserved";
 		break;
 	    case "cs":
-		document.getElementById("navbarlink1text").textContent = "O nás";
-		document.getElementById("navbarlink2text").textContent = "Služby";
-		document.getElementById("navbarlink3text").textContent = "Ceník";
-		document.getElementById("navbarlink4text").textContent = "Reference";
-
-		document.getElementById("navbarbuttontext").textContent = "Kontaktujte nás";
-
 		document.getElementById("footerTOStext").textContent = "Smluvní podmínky";
 		document.getElementById("footerPPtext").textContent = "Ochrana osobních údajů";
 		document.getElementById("footerrightstext").textContent = "Všechna práva vyhrazena";
